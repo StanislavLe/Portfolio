1a26e903e53e96c40e113799946e9372878e2474
Stanislav Levin <stanislav2708@outlook.de>
2025-07-17 19:38:52 +0200

    add bootstrap and angular material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
@@ -10,7 +12,7 @@ import { SkillsetComponent } from './skillset/skillset.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SkillsetComponent, ReferencesComponent, RouterOutlet, AboutMeComponent, ContactMeComponent, PortfolioComponent],
+  imports: [CommonModule, HeaderComponent, SkillsetComponent, HeroSectionComponent, ReferencesComponent, RouterOutlet, AboutMeComponent, ContactMeComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
