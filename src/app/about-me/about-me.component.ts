dafd9b1a2ead15047402c01ff31eca63f9129c5d
Stanislav Levin <stanislav2708@outlook.de>
2025-08-05 18:04:37 +0200

    add about me secret section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
