bb638a48756787b21496971763b7e25c74926861
Stanislav Levin <stanislav2708@outlook.de>
2025-10-04 12:21:06 +0200

    fix impressum styles

--- src/app/legal/impressum/impressum.component.ts
+++ src/app/legal/impressum/impressum.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
